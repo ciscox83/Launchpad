443ab90d501130ae1310056c61953771a8028124
Christian Sisti <ciscox83@Christians-MacBook-Pro.local>
2017-12-30 18:23:14 +0100

    Created first failing test

---
+++ src/test/java/LaunchpadTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+public class LaunchpadTest {
+
+  @Test
+  public void launchpadTest() {
+    whenLaunchpadIsRunning();
+  }
+
+  private void whenLaunchpadIsRunning() {
+    Launchpad launchpad = new Launchpad();
+    launchpad.start();
+  }
+
+}
